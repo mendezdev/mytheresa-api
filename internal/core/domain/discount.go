7dd4327e3bc68e934b76e1f93edcb074c721bbcc
Pablo Mendez <mendez.developer@gmail.com>
2021-08-07 18:27:09 -0300

    add discount logic

--- internal/core/domain/discount.go
+++ internal/core/domain/discount.go
@@ -1,16 +1,19 @@
 package domain
 
-type DiscountResult struct {
-	Value     int64
-	Percetage int
-}
+const (
+	EqualOperator = "equal"
+)
+
+type DiscountResult int64
+
+type DiscountRequest map[string]interface{}
 
 type Discount struct {
 	FieldName string      `json:"field_name"`
 	Operator  string      `json:"operator"`
 	Value     interface{} `json:"value"`
+	ValueType string      `json:"value_type"`
 	Priority  int         `json:"priority"`
-	ValueType interface{} `json:"value_type"`
 	Apply     int64       `json:"apply"`
 }
 
@@ -18,6 +21,42 @@ func NewDiscount() Discount {
 	return Discount{}
 }
 
-func (d Discount) ApplyDiscount(p Product) (*DiscountResult, error) {
-	return nil, nil
+func (d Discount) GetDiscount(dr DiscountRequest) (*DiscountResult, error) {
+	if len(dr) == 0 {
+		return nil, nil
+	}
+
+	drVal, ok := dr[d.FieldName]
+	if !ok {
+		return nil, nil
+	}
+
+	isTypeOk := checkInterfaceType(drVal, d.ValueType)
+	if !isTypeOk {
+		return nil, nil
+	}
+
+	var discountResult DiscountResult
+
+	switch d.Operator {
+	case EqualOperator:
+		if d.Value == drVal {
+			discountResult = DiscountResult(d.Apply)
+		}
+	}
+
+	return &discountResult, nil
+}
+
+func checkInterfaceType(val interface{}, valueType string) bool {
+	switch valueType {
+	case "long":
+		_, ok := val.(int64)
+		return ok
+	case "decimal":
+		_, ok := val.(float64)
+		return ok
+	default:
+		return false
+	}
 }
