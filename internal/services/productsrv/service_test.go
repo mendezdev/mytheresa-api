83dc8c8d1ea49a12743c565e1c8b8274af9d8c8c
Pablo Mendez <mendez.developer@gmail.com>
2021-08-10 11:55:41 -0300

    added product srv tests

--- internal/services/productsrv/service_test.go
+++ internal/services/productsrv/service_test.go
@@ -1,6 +1,7 @@
 package productsrv
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 
@@ -92,6 +93,79 @@ func TestProductOk(t *testing.T) {
 		if result.Category == bootProduct.Category {
 			assert.Equal(t, *result.Price.DiscountPercentage, fmt.Sprintf("%d%s", d1.Apply, "%"))
 			assert.Equal(t, *result.Price.Final, int64(21000))
+		} else {
+			assert.Nil(t, result.Price.DiscountPercentage)
+			assert.Nil(t, result.Price.Final)
 		}
 	}
+}
+
+func TestProductDiscountError(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockProductRepo := ports.NewMockProductRepository(mockCtrl)
+	mockDiscountSrv := ports.NewMockDiscountService(mockCtrl)
+
+	productSrv := New(mockProductRepo, mockDiscountSrv)
+
+	discountErr := errors.New("discount error")
+	mockDiscountSrv.
+		EXPECT().
+		GetDiscounts().
+		Return(nil, discountErr).
+		Times(1)
+
+	pf := domain.ProductFilter{
+		Category: "boots",
+	}
+
+	bootProduct := domain.Product{
+		Sku:      "00001",
+		Name:     "p1",
+		Category: "boots",
+		Price:    30000,
+	}
+
+	shoeProduct := domain.Product{
+		Sku:      "00002",
+		Name:     "p2",
+		Category: "shoes",
+		Price:    12000,
+	}
+
+	productsExpected := []domain.Product{bootProduct, shoeProduct}
+	mockProductRepo.
+		EXPECT().
+		GetByCategory(pf.Category, productsLimit, pf.LesstThan).
+		Return(productsExpected, nil).
+		Times(1)
+
+	_, err := productSrv.GetProductsByCategory(pf)
+	assert.NotNil(t, err)
+}
+
+func TestProductByCategoryReturnsEmptyResult(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockProductRepo := ports.NewMockProductRepository(mockCtrl)
+	mockDiscountSrv := ports.NewMockDiscountService(mockCtrl)
+
+	productSrv := New(mockProductRepo, mockDiscountSrv)
+
+	productsExpected := make([]domain.Product, 0)
+	pf := domain.ProductFilter{
+		Category: "boots",
+	}
+	mockProductRepo.
+		EXPECT().
+		GetByCategory(pf.Category, productsLimit, pf.LesstThan).
+		Return(productsExpected, nil).
+		Times(1)
+
+	result, err := productSrv.GetProductsByCategory(pf)
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+	assert.Len(t, result, 0)
 }
